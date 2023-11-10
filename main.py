81658f73c0f38a8c05b0bfeb11e864915e798506
CaptianSiix <53707930+CaptianSiix@users.noreply.github.com>
2023-11-10 13:50:00 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+def print_BMI():
+    weight = input("Please tell me your weight in kilograms!")
+    height = input("Please tell me your height in meters!")
+
+##def print_BMI(BMI):
+
+    fweight = float(weight)
+    fheight = float(height)
+##print(fheight, fweight)
+    BMI = (fweight/fheight**2)
+    fBMI = float(BMI)
+    if (BMI>0):
+        if (BMI <= 16):
+            print("you are severely underweight")
+        elif (BMI <= 18.5):
+            print("you are underweight")
+        elif (BMI <= 25):
+            print("you are Healthy")
+        elif (BMI <= 30):
+            print("you are overweight")
+        else:
+            print("you are severely overweight")
+
+    print("Your BMI is", BMI)
+
+print_BMI()
